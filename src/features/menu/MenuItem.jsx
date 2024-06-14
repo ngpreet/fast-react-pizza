ff72006e326afa75aac5d78bb9966691c087637a
ngpreet <ng.navpreet@gmail.com>
2024-06-14 08:40:22 +0530

    menu styling

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,4 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 import * as PropTypes from "prop-types";
 
@@ -5,13 +6,24 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex gap-4 py-2">
+      <img
+        className={`h-28 ${soldOut ? "grayscale" : ""}`}
+        src={imageUrl}
+        alt={name}
+      />
+      <div className="flex grow flex-col">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex justify-between text-sm uppercase">
+          {!soldOut ? (
+            <p>{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="font-bold text-stone-400">Sold out</p>
+          )}
+          <Button type="small">Add to cart</Button>
         </div>
       </div>
     </li>
@@ -19,7 +31,7 @@ function MenuItem({ pizza }) {
 }
 
 MenuItem.propTypes = {
-  pizza: PropTypes.object
-}
+  pizza: PropTypes.object,
+};
 
 export default MenuItem;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,23 @@
 import * as PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className = `inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800
-    transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed`;
+function Button({ children, disabled, to, type }) {
+  const base = `inline-block rounded-full bg-yellow-400 font-semibold uppercase text-stone-800
+    transition-colors duration-300 hover:bg-yellow-300 cursor-pointer focus:bg-yellow-300 focus:outline-none
+    focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed`;
+  const styles = {
+    primary: base + " px-4 py-3",
+    small: base + " px-2 py-1 text-xs",
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
@@ -22,6 +27,7 @@ Button.propTypes = {
   children: PropTypes.node,
   disabled: PropTypes.bool,
   to: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default Button;
