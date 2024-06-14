656aa4be46d766f4ccbf7408fd1ab26046a34860
ngpreet <ng.navpreet@gmail.com>
2024-06-14 09:31:03 +0530

    cart styling

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,6 +8,9 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3",
     small: base + " px-2 py-1 text-xs",
+    secondary: `inline-block rounded-full font-semibold uppercase text-stone-400 border-2 border-stone-300
+    transition-colors duration-300 hover:bg-stone-300 hover:text-stone-600 cursor-pointer focus:bg-stone-300 focus:outline-none
+    focus:ring focus:ring-stone-300 focus:ring-offset-1 disabled:cursor-not-allowed px-4 py-3`,
   };
 
   if (to)
